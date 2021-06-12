a175c966d05d4bc6f1c61fdc636f644fa8e62c04
minhajhasan-dev <minhajhasan.dev@gmail.com>
2021-06-12 23:29:44 +0600

    setTimeout & setInterval

---
+++ timeout.js
@@ -0,0 +1,18 @@
+function doSomething(){
+    console.log(3333);
+}
+console.log(2222);
+// setTimeout(function(){
+//     console.log('Thanks for waiting!')
+// }, 5000);
+
+setInterval(function(){
+    console.log('doing it');
+}, 5000)
+
+
+console.log(4444);
+console.log(5555);
+console.log(6666);
+console.log(7777);
+console.log(8888);
